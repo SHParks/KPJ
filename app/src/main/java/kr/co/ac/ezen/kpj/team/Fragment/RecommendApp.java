bd3064698c2e82198d9f2e58f68a4e18e3de9d24
Administrator <ssww121@naver.com>
2018-02-09 13:14:32 +0900

    추천앱

--- app/src/main/java/kr/co/ac/ezen/kpj/team/Fragment/RecommendApp.java
+++ app/src/main/java/kr/co/ac/ezen/kpj/team/Fragment/RecommendApp.java
@@ -4,20 +4,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
 import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import kr.co.ac.ezen.kpj.team.R;
 import kr.co.ac.ezen.kpj.team.Utility.InstallCheck;
 
 public class RecommendApp extends Fragment {
+    ArrayList<String> packageList = new ArrayList<>();
+    ArrayList<RelativeLayout> layoutList = new ArrayList<>();
+    @BindView(R.id.youtube)
+    RelativeLayout youtube;
+    @BindView(R.id.kakaoTalk)
+    RelativeLayout kakaoTalk;
+    @BindView(R.id.kakaoMap)
+    RelativeLayout kakaoMap;
+    @BindView(R.id.metro)
+    RelativeLayout metro;
+    @BindView(R.id.naver)
+    RelativeLayout naver;
+    @BindView(R.id.addressBook)
+    RelativeLayout addressBook;
+    @BindView(R.id.dodolkeyboard)
+    RelativeLayout dodolkeyboard;
 
 
     @Nullable
@@ -38,9 +59,41 @@ public class RecommendApp extends Fragment {
             scrollView.setTouchInterceptionViewGroup((ViewGroup) parentActivity.findViewById(R.id.root));
             scrollView.setScrollViewCallbacks((ObservableScrollViewCallbacks) parentActivity);
         }
+        packageList.add("com.google.android.youtube");
+        packageList.add("com.kakao.talk");
+        packageList.add("net.daum.android.map");
+        packageList.add("teamDoppelGanger.SmarterSubway");
+        packageList.add("com.nhn.android.search");
+        packageList.add("com.nhn.android.addressbookbackup");
+        packageList.add("com.google.android.inputmethod.korean");
+
+        layoutList.add(youtube);
+        layoutList.add(kakaoTalk);
+        layoutList.add(kakaoMap);
+        layoutList.add(metro);
+        layoutList.add(naver);
+        layoutList.add(addressBook);
+        layoutList.add(dodolkeyboard);
+
+
         return rootview;
     }
-    @OnClick({R.id.downYoutube, R.id.downKakaoTalk, R.id.downKakaoMap, R.id.downMetro, R.id.downNaver, R.id.downAddressBook})
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        for(int i=0; i < packageList.size(); i++){
+            Log.d("alc",layoutList.get(i)+" ");
+            if (InstallCheck.searchAppPackage(getActivity(),packageList.get(i))){
+                layoutList.get(i).setVisibility(View.GONE);
+                Log.d("alc",layoutList.get(i)+"숨김");
+            }else {
+                layoutList.get(i).setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @OnClick({R.id.downYoutube, R.id.downKakaoTalk, R.id.downKakaoMap, R.id.downMetro, R.id.downNaver, R.id.downAddressBook,R.id.downdodolkeyboard})
     public void onclick(View view){
         String packageName="";
         switch (view.getId()){
@@ -62,11 +115,17 @@ public class RecommendApp extends Fragment {
             case R.id.downAddressBook:
                 packageName = "com.nhn.android.addressbookbackup";
                 break;
+            case R.id.downdodolkeyboard:
+                packageName = "com.google.android.inputmethod.korean";
+                break;
+
+
         }
-        if (InstallCheck.searchAppPackage(getActivity(),packageName)){
-            InstallCheck.executeLocalAppPackage(getActivity(),packageName);
-        } else {
-            InstallCheck.executeStoreAppPackage(getActivity(),packageName);
-        }
+//        if (InstallCheck.searchAppPackage(getActivity(),packageName)){
+//            InstallCheck.executeLocalAppPackage(getActivity(),packageName);
+//        } else {
+//            InstallCheck.executeStoreAppPackage(getActivity(),packageName);
+//        }
+        InstallCheck.executeStoreAppPackage(getActivity(),packageName);
     }
 }
